c5cd64466f2efc6b28513486335edaf7d8172f97
Qonda <138157862+Qonda3@users.noreply.github.com>
2024-05-10 13:05:40 +0200

    refactor

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const TIME_CONVERSION = 3600
+const CONVERSION_FACTOR = 12960
 /**
  * Debugging Guide
  * 1. Make the code more readable
@@ -6,37 +8,56 @@
  */
 
 // Given Parameters
-const initialVelocity = 10000; // velocity (km/h)
-const acceleration = 3; // acceleration (m/s^2)
-const timeInSeconds = 3600; // seconds (1 hour)
-const distance = 0; // distance (km)
-const initialFuel = 5000; // remaining fuel (kg)
-const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
-
 const quantities = {
-  velocity: initialVelocity,
-  acceleration: acceleration,
-  time: timeInSeconds,
-}
-const finalDistance = distance + (initialVelocity * timeInSeconds / 3600) //calcultes new distance
-const remainingFuel = initialFuel - (fuelBurnRate * timeInSeconds) //calculates remaining fuel
+  timeInSeconds: 3600, // seconds (1 hour)
 
+  initialVelocity: 10000, // velocity (km/h)
+  acceleration: 3, // acceleration (m/s^2)
+  initialDistance: 0, // distance (km)
+
+  initialFuel: 5000, // remaining fuel (kg)
+  fuelBurnRate: 0.5, // fuel burn rate (kg/s)
+}
+
+
+//calculates remaining fuel
+const calcRemainingFuel = (quantities) => {
+  if (!quantities) throw new Error('"quantities" is required to calc remaining fuel')
+
+  const {initialFuel, fuelBurnRate, timeInSeconds} = quantities
+  if (!initialFuel) throw new Error('"initail fuel" is required to calc remaining fuel')
+  if (!fuelBurnRate) throw new Error('"fuel burn rate" is required to calc remaining fuel')
+  if (!timeInSeconds) throw new Error('"timeInSeconds" is required to calc remaining fuel')
+  return initialFuel - (fuelBurnRate * timeInSeconds)
+}
+//calcultes new distance
+const calcNewDistance = (quantities) => {
+  if (!quantities) throw new Error('"quantities" is required to calc new distance')
+
+  const {initialDistance, initialVelocity, timeInSeconds} = quantities
+
+  if (initialDistance === null || initialDistance < 0) throw new Error('"distance" is required to calc new distance and cannot be less than zero')
+  if (!initialVelocity) throw new Error('"initial velocity" is required to calc new distance')
+  if (!timeInSeconds) throw new Error('"timeInSeconds" is required to calc new distance')
+
+  return initialDistance + (initialVelocity * timeInSeconds / TIME_CONVERSION)
+}
 // Pick up an error with how the function below is called and make it robust to such errors
 const calcNewVel = (quantities) => {
 
   if (!quantities) throw new Error('"quantities" is required to calc final velocity')
-  const {velocity, acceleration, time} = quantities
-  if (!velocity) throw new Error('"velocity" is required to calc final speed')
+  const {initialVelocity, acceleration, timeInSeconds} = quantities
+  if (!initialVelocity) throw new Error('"initial velocity" is required to calc final speed')
   if (!acceleration) throw new Error('"constant acceleration" is required to calc final speed')
-  if (!time) throw new Error('"time" is required to calc final speed')
-  const timeInHours = time  / 3600
-  const accelerationInKmPerHour = acceleration * 12960
-  return velocity + (accelerationInKmPerHour * timeInHours)
+  if (!timeInSeconds) throw new Error('"timeInSeconds" is required to calc final speed')
+  const timeInHours = timeInSeconds  / TIME_CONVERSION
+  const accelerationInKmPerHour = acceleration * CONVERSION_FACTOR
+  return initialVelocity + (accelerationInKmPerHour * timeInHours)
 }
 
 const finalVelocity = calcNewVel(quantities) //calculates new velocity based on acceleration
-
-
+const finalDistance = calcNewDistance(quantities) 
+const remainingFuel = calcRemainingFuel(quantities)
 
 console.log(`Corrected New Velocity: ${finalVelocity} km/h`);
 console.log(`Corrected New Distance: ${finalDistance} km`);
